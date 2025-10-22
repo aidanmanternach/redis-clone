d0399b3bf0e6b4d51730f03517ff6cbcb7d7fcf3
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2025-10-22 15:49:04 -0700

    Create Append Only File for data persistence

--- main.go
+++ main.go
@@ -16,6 +16,26 @@ func main() {
 		return
 	}
 
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
+
 	for {
 		// Listen for connections
 		conn, err := l.Accept()
@@ -24,11 +44,11 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
+		go handleConnection(conn, aof)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, aof *Aof) {
 	defer conn.Close()
 
 	for {
@@ -59,6 +79,10 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)

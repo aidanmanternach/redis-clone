e905ec89221e51c6ef269ed385f024c1eb1eb5b5
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2025-10-22 14:41:41 -0700

    Implement HGETALL functionality

--- handlers.go
+++ handlers.go
@@ -8,6 +8,7 @@ var Handlers = map[string]func([]Value) Value {
 	"GET": get,
 	"HSET": hset,
 	"HGET": hget,
+	"HGETALL": hgetall,
 }
 
 func ping(args []Value) Value {
@@ -93,4 +94,27 @@ func hget(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: value}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hgetall' command"}
+	}
+
+	hash := args[0].bulk
+	HSETsMu.RLock()
+	value, ok := HSETs[hash]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	values := []Value{}
+	for k, v := range value {
+		values = append(values, Value{typ: "bulk", bulk: k})
+		values = append(values, Value{typ: "bulk", bulk: v})
+	}
+
+	return Value{typ: "array", array: values}
 }
